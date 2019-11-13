98976e10ec2330318b6bc797d2b7aa69948e809a
Louis Jean <ljean@etud.insa-toulouse.fr>
2019-11-13 17:10:48 +0100

    Add TP1 first part

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Travaux Pratiques d'Apprentissage Supervisé à l'Institut National des Sciences Appliquées de Toulouse.
 
+## Travail pratique numéro 1
+
+Executer à l'aide de la commande suivante (affiche les options disponibles):
+
+```
+TP1/tp1.py --help
+```
--- TP1-knn/tp1.py
+++ TP1-knn/tp1.py
@@ -1,1 +1,23 @@
-# knn
+#!/usr/bin/python3
+
+import argparse
+
+import matplotlib.pyplot as plt
+
+# Argument parsing
+parser = argparse.ArgumentParser(description="TP1 knn")
+parser.add_argument("--visualize", type=int, help="Visualize data")
+
+args = parser.parse_args()
+
+# Get dataset
+import sklearn.datasets as dsets
+
+mnist = dsets.fetch_openml("mnist_784")
+
+# Visualize data
+if args.visualize != None:
+    images = mnist.data.reshape((-1, 28, 28))
+    plt.imshow(images[args.visualize], cmap=plt.cm.gray_r,interpolation="nearest")
+    print(mnist.target[args.visualize])
+    plt.show()
