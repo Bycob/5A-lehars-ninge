d413f19da4058965fbfd86a08472e605ca9c352d
MaxenceHanin <hanin@etud.insa-toulouse.fr>
2019-11-19 17:57:10 +0100

    Implementing ann method with default parameters

--- TP2-ann/tp2.py
+++ TP2-ann/tp2.py
@@ -8,8 +8,15 @@ import matplotlib.pyplot as plt
 
 # Argument parsing
 parser = argparse.ArgumentParser(description="TP2 knn")
-parser.add_argument("--visualize_data", type=int, metavar="data_id", help="Visualize image n and display corresponding class")
-parser.add_argument("--compare", choices=["neighbors", "split", "distance"], help="Make one parameter vary to find the best outcome")
+parser.add_argument("--visualize_data", type=int, metavar="data_id"
+                    , help="Visualize image n and display corresponding class")
+#parser.add_argument("--compare", choices=["neighbors", "split", "distance"]
+# , help="Make one parameter vary to find the best outcome")
+parser.add_argument("--hidden_layer_sizes", type=list, default= [50], help="Tuple corresponding to the number of neural for each layer")
+parser.add_argument("--activation", choices=["identity", "logistic", "tanh", "relu"], default="relu",
+                    help="Activation function for the hidden layer")
+parser.add_argument("--solver", choices=["ldfgs", "sgd", "adam"], default="adam", help="The solver for weight optimization")
+parser.add_argument("--alpha", type=float,default=0.0001, help="L2 penalty (regularization term) parameter")
 
 args = parser.parse_args()
 
@@ -28,3 +35,38 @@ def visualize(data, target):
 if args.visualize_data != None:
     visualize(mnist.data[args.visualize_data], mnist.target[args.visualize_data])
     sys.exit(0)
+
+#MLPClassifier
+from sklearn.model_selection import train_test_split
+import sklearn.neural_network as n_network
+import numpy as np
+
+
+#Create dataset
+mnist_size = len(mnist.data)
+subset_data = mnist.data
+subset_target = mnist.target
+
+xtrain, xtest, ytrain, ytest = train_test_split(subset_data, subset_target, train_size=49000/mnist_size)
+print("train size = %d, test size = %d" % (len(xtrain), len(xtest)))
+
+def classifier(hidden_layer_sizes,activation, solver, alpha):
+    print("Training with %d hidden layers, with the following neural number: %s" % (len(hidden_layer_sizes)
+                                                                                    ,str(hidden_layer_sizes)))
+    mlpc = n_network.MLPClassifier(hidden_layer_sizes=tuple(hidden_layer_sizes), activation=activation, solver=solver
+                                   ,alpha=alpha)
+    t = time.time()
+    mlpc.fit(xtrain, ytrain)
+    training_t = time.time()
+    score = mlpc.score(xtest, ytest)
+    scoring_t = time.time()
+
+    print("Score is %f with %d hidden layers, with the following neural number: %s" % (score, len(hidden_layer_sizes)
+                                                                                    ,str(hidden_layer_sizes)))
+    print("Training time %f ms, scoring time %f ms" % ((training_t - t) * 1000, (scoring_t - training_t) * 1000))
+    return mlpc, score
+
+
+# Run a normal test without making anything vary
+mlpc0 = classifier(args.hidden_layer_sizes, args.activation, args.solver, args.alpha)[0]
+visualize(xtest[0], mlpc0.predict(xtest[0].reshape((1, -1))))
